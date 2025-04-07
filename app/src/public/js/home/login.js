b3c69e388a2a9175fa7267d510a3c63bcac4ad00
Lee Seung Hee <kye1281@naver.com>
2025-04-07 17:02:15 +0900

    DOM을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,13 @@
 "use strict";
 
-console.log("hhhh");
+// DOM : Document Object Model, 일종의 인터페이스
+const id = document.querySelector("#id"),
+  psword = document.querySelector("#psword"),
+  loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", () => {
+  const req = {
+    id: id.value,
+    psword: psword.value
+  };
+});
